a23eca2da88d16f5070e686199eeaa0ffa2eabef
Belt0M <belongtomars@gmail.com>
2024-05-26 21:28:08 +0200

    Add the Device manager

---
+++ IotHub.Agent/IoTDevice.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.Devices.Client;
+
+namespace IndustrialIoT
+{
+    enum Errors
+    {
+        EmergencyStop = 1,
+        PowerFailure = 2,
+        SensorFailue = 4,
+        Unknown = 8
+    }
+
+    public class IoTDevice
+    {
+        private readonly DeviceClient client;
+        private OpcManager opcManager;
+
+        public IoTDevice(DeviceClient deviceClient, OpcManager opcManager)
+        {
+            client = deviceClient;
+            this.opcManager = opcManager;
+        }
+    }
+}
