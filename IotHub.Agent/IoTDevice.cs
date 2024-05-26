9c2d5d3b16e5af546988d434e634ca21d4016c4a
Belt0M <belongtomars@gmail.com>
2024-05-26 21:30:20 +0200

    Add a device twin

--- IotHub.Agent/IoTDevice.cs
+++ IotHub.Agent/IoTDevice.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
 using Newtonsoft.Json;
 using Opc.UaFx.Client;
 using System.Net.Mime;
@@ -48,8 +49,74 @@ namespace IndustrialIoT
             eventMessage.ContentEncoding = "utf-8";
 
             await client.SendEventAsync(eventMessage);
+
+            await UpdateTwinAsync(opcManager.client);
         }
 
         #endregion
+
+        #region Device Twin
+
+        public async Task UpdateTwinAsync(OpcClient opcClient)
+        {
+            StringBuilder errorBuilder = new StringBuilder();
+
+            int errors = (int)opcClient.ReadNode($"ns=2;s=Device {Program.deviceNumber}/DeviceError").Value;
+
+            if ((errors & Convert.ToInt32(Errors.Unknown)) != 0)
+            {
+                errorBuilder.Append("Unknown ");
+            }
+
+            if ((errors & Convert.ToInt32(Errors.SensorFailue)) != 0)
+            {
+                errorBuilder.Append("SensorFailure ");
+            }
+
+            if ((errors & Convert.ToInt32(Errors.PowerFailure)) != 0)
+            {
+                errorBuilder.Append("PowerFailure ");
+            }
+
+            if ((errors & Convert.ToInt32(Errors.EmergencyStop)) != 0)
+            {
+                errorBuilder.Append("Emergency stop ");
+            }
+
+            string errorsString = errorBuilder.ToString();
+
+            var reportedProperties = new TwinCollection();
+
+            reportedProperties["productionRate"] = opcClient.ReadNode($"ns=2;s=Device {Program.deviceNumber}/ProductionRate").Value;
+            reportedProperties["deviceErrors"] = errorsString;
+
+            await client.UpdateReportedPropertiesAsync(reportedProperties);
+        }
+
+        private async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object userContext)
+        {
+            int value = desiredProperties["productionRate"];
+
+            await opcManager.SetProductionRate(value);
+
+            TwinCollection reportedProperties = new TwinCollection();
+
+            reportedProperties["productionRate"] = value;
+
+            await client.UpdateReportedPropertiesAsync(reportedProperties).ConfigureAwait(false);
+        }
+
+        #endregion
+
+        public async Task InitializeHandlers()
+        {
+            await client.SetMethodHandlerAsync("EmergencyStop", EmergencyStopHandler, client);
+
+            await client.SetMethodHandlerAsync("ResetErrorStatus", ResetErrorStatusHandler, client);
+
+            await client.SetMethodDefaultHandlerAsync(DefaultServiceHandler, client);
+
+            await client.SetDesiredPropertyUpdateCallbackAsync(OnDesiredPropertyChanged, client);
+        }
     }
 }
