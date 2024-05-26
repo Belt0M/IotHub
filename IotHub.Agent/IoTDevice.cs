5688e02479ce16b5bbb2816c1a9e00a85f8b2291
Belt0M <belongtomars@gmail.com>
2024-05-26 21:29:30 +0200

    Add a D2C feature

--- IotHub.Agent/IoTDevice.cs
+++ IotHub.Agent/IoTDevice.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using Opc.UaFx.Client;
+using System.Net.Mime;
+using System.Text;
 
 namespace IndustrialIoT
 {
@@ -20,5 +24,32 @@ namespace IndustrialIoT
             client = deviceClient;
             this.opcManager = opcManager;
         }
+
+
+        #region Sending Messages
+
+        public async Task SendMessages(OpcClient opcClient, int deviceNumber)
+        {
+            var data = new
+            {
+                productionStatus = opcClient.ReadNode($"ns=2;s=Device {deviceNumber}/ProductionStatus").Value,
+                workorderId = opcClient.ReadNode($"ns=2;s=Device {deviceNumber}/WorkorderId").Value,
+                goodCount = opcClient.ReadNode($"ns=2;s=Device {deviceNumber}/GoodCount").Value,
+                badCount = opcClient.ReadNode($"ns=2;s=Device {deviceNumber}/BadCount").Value,
+                temperature = opcClient.ReadNode($"ns=2;s=Device {deviceNumber}/Temperature").Value
+            };
+
+            var dataString = JsonConvert.SerializeObject(data);
+
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataString));
+
+            eventMessage.ContentType = MediaTypeNames.Application.Json;
+
+            eventMessage.ContentEncoding = "utf-8";
+
+            await client.SendEventAsync(eventMessage);
+        }
+
+        #endregion
     }
 }
